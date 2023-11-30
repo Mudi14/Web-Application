737eaf1f06eaee588499e633ad3b095bf72308ad
Mudiaga Ejayeriese <106126888+Mudi14@users.noreply.github.com>
2023-11-29 20:52:23 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId:         'us-east-2_ZmGcMYAEu',         // e.g. us-east-2_uXboG5pAb
-        userPoolClientId:   '7t472rbg838c6834fnui77s7lg',         // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId:         'us-east-2_HhkoUiPqb',         // e.g. us-east-2_uXboG5pAb
+        userPoolClientId:   '711a5f01jk3rg1a5i5gg6dfl51',         // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region:             'us-east-2'          // e.g. us-east-2
     },
     api: {
-        invokeUrl:          'https://3c60w53kxj.execute-api.us-east-2.amazonaws.com/prod'          // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl:          ''          // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
